28fb0bc27c4591066657e463c081ab6d3ae90985
Lin XiaoYu <ttly20@qq.com>
2019-07-10 13:34:44 +0800

    删除多余文件

--- modules/directory.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const directory = new mongoose.Schema({
-    directory: { type: String },
-    notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "notes" }]
-})
-
-module.exports = mongoose.model("directory", directory)
--- modules/tags.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const tags = new mongoose.Schema({
-    lable: { type: String },
-    notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "notes" }]
-})
-
-module.exports = mongoose.model("tags", tags)
